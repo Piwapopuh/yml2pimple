3b09400e710ed6ffb66fef81d588481fde5e1a33
Thomas Dräger <td7650@gmx.de>
2015-06-10 21:43:13 +0200

    added upper case A-Z to PropertyAccessPimpleNormalizer matcher

--- src/Normalizer/PropertyAccessPimpleNormalizer.php
+++ src/Normalizer/PropertyAccessPimpleNormalizer.php
@@ -26,36 +26,33 @@ class PropertyAccessPimpleNormalizer
      * @param  string $value
      * @return string
      */
-    public function normalize($value)
+    public function normalize($value, $container)
     {
 		if (!is_string($value)) {
 			return $value;
 		}
 		
-        if (preg_match('{^%([a-z0-9_.\[\]]+)%$}', $value, $match)) {
-			if ($this->accessor->isReadable($this->pimple, $match[1])) {
-				return $this->accessor->getValue($this->pimple, $match[1]);
+        if (preg_match('{^%([a-zA-Z0-9_.\[\]]+)%$}', $value, $match)) {
+			if ($this->accessor->isReadable($container, $match[1])) {
+				return $this->accessor->getValue($container, $match[1]);
 			} 
 			return $match[0];
         }
 
-        $result = preg_replace_callback('{%%|%([a-z0-9_.\[\]]+)%}', array($this, 'callback'), $value, -1, $count);
+		$callback = function($matches) use ($container)
+		{
+			if (!isset($matches[1])) {
+				return '%%';
+			}
+			if ($this->accessor->isReadable($container, $matches[1])) {
+				return $this->accessor->getValue($container, $matches[1]);
+			}
+			return $matches[0];
+		};		
+		
+        $result = preg_replace_callback('{%%|%([a-zA-Z0-9_.\[\]]+)%}', array($this, $callback), $value, -1, $count);
 
         return $count ? $result : $value;
     }
 
-    /**
-     * @param  array $matches
-     * @return mixed
-     */
-    protected function callback($matches)
-    {
-        if (!isset($matches[1])) {
-            return '%%';
-        }
-		if ($this->accessor->isReadable($this->pimple, $matches[1])) {
-			return $this->accessor->getValue($this->pimple, $matches[1]);
-		}
-		return $matches[0];
-    }
 }
